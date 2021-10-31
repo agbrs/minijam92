93849ac7c7f6e704af67cc3854c5e4b3dde58b20
Corwin Kuiper <corwin@kuiper.dev>
2021-10-31 15:12:36 +0000

    add release files

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cargo build --release
+
+mkdir -p target/final-zip
+rm -rf traget/final-zip/the-purple-night
+cp -rv release-data/the-purple-night target/final-zip
+
+arm-none-eabi-objcopy -O binary target/thumbv4t-none-eabi/release/minijam92 target/final-zip/the-purple-night/thepurplenight.gba
+gbafix -p -tPURPLENIGHT -cPURP -mGC target/final-zip/the-purple-night/thepurplenight.gba
+
+cp screenshot.png target/final-zip/the-purple-night
+
+rm -f target/thepurplenight.zip
+(cd target/final-zip && zip -r ../thepurplenight.zip the-purple-night)
